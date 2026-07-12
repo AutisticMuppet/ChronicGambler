5ae9a41b9e05d6ff2d0b8f1b3848b1e948a1b592
Autistic_Muppet <125842438+AutisticMuppet@users.noreply.github.com>
2026-07-12 18:21:09 +1000

    Attempt #7 of Fixing Result based messages
    Nevermind. #6 did not work.

--- lib/lichess_bot.py
+++ lib/lichess_bot.py
@@ -826,11 +826,11 @@ def get_greeting(greeting: str, greeting_cfg: Configuration, keyword_map: defaul
 def get_result_greeting(base_key: str, greeting_cfg: Configuration, keyword_map: defaultdict[str, str],
                         game: model.Game) -> str:
     "Send a greeting to the opponent based on results (THIS CAUSED ME SO MUCH PAIN TO PROGRAM GSVVAVJKAGVJDGYJVDYVGD)"
-
+    
     if game.result == "1/2-1/2":
         # ChronicGambler draws (The opponent cheated and still could not win.)
         candidate_keys = [f"{base_key}_draw", base_key]
-    elif (game.is_white and game.result == "1-0") or (not game.is_white and game.result == "0-1"):
+    elif (game.white.name != "ChronicGambler" and game.result == "1-0") or (game.black.name != "ChronicGambler" and game.result == "0-1"):
         # ChronicGambler loses (Rigged Game)
         candidate_keys = [f"{base_key}_win", base_key]
     else:
